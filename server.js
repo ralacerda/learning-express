015ec0330ff31e100fb3d7831b7e5855f4fdd0d8
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-03-28 19:05:23 -0300

    feat: first server version

---
+++ server.js
@@ -0,0 +1,8 @@
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Curso de node");
+});
+
+server.listen(3000);
