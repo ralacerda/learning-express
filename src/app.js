1fa5266b4ac0c111b70c181dca1ae5a6ac218e9f
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-03-29 12:49:35 -0300

    feat: new function to find book and route to put

--- src/app.js
+++ src/app.js
@@ -7,12 +7,21 @@ const livros = [
   { id: 2, titulo: "O Hobbit" },
 ];
 
+function findBook(id) {
+  return livros.findIndex((livro) => livro.id == id);
+}
+
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node");
 });
 
 app.get("/livros", (req, res) => {
   res.status(200).json(livros);
+});
+
+app.put("/livros/:id", (req, res) => {
+  const index = findBook(req.params.id);
+  livros[index].titulo = req.body.titulo;
 });
 
 export default app;
