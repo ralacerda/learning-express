7ccbfbc5dcadf51ec4f22ad206825d435819c7bb
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-04-06 08:45:56 -0300

    fix: removing `.js`

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "#routes/index.js";
-import db from "./config/connectAtlas.js";
+import db from "./config/connectAtlas";
 
 db.on("error", () => console.error("Database conection error"));
 db.once("open", () => console.info("Successfully connected to the database"));
